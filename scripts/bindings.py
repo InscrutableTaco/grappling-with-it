f6cc554ef105d941394087b6723a194e5b8be02e
Tim Likarish Ellis <likarish@gmail.com>
2025-05-24 16:14:45 -0400

    [Bindings] Add joystick/controller support

--- scripts/bindings.py
+++ scripts/bindings.py
@@ -7,6 +7,15 @@ class Bindings:
     def __init__(self, game):
         self.game = game
 
+        pygame.joystick.init()
+        self.joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
+        for joystick in self.joysticks:
+            print("Initializing joystick")
+            joystick.init()
+
+        # Ignore small movements
+        self.joystick_deadzone = 0.2  
+
         def press_left(self, game):
             game.movement[0] = True
         def depress_left(self, game):
@@ -38,10 +47,21 @@ class Bindings:
             'jump': [{pygame.K_SPACE, pygame.K_UP, pygame.K_w}, press_jump, depress_jump],
             'grapple': [{pygame.K_x, pygame.K_g}, press_grapple, depress_grapple],
             'reset': [{pygame.K_r}, press_reset, depress_reset],
-        }   
+        }
+
+        self.controller_bindings = {
+            'left': [('axis', 0, -1)],  # Left on left analog stick
+            'right': [('axis', 0, 1)],  # Right on left analog stick
+            'jump': [('button', 0)],    # A button (Switch Pro) / A button (Xbox) / X button (PlayStation)
+            'grapple': [('button', 1)], # B button (Switch Pro) / B button (Xbox) / Circle button (PlayStation)
+            'reset': [('button', 6)],   # Plus/Back/Select button
+        }
+        
+        # For tracking axis movements
+        self.axis_states = {}
 
     def read_input(self, game, event):
-        
+        # Keyboard events
         if event.type == pygame.KEYDOWN:
             for action in self.bindings_map:
                 if event.key in self.bindings_map[action][0]:
@@ -51,3 +71,37 @@ class Bindings:
             for action in self.bindings_map:
                 if event.key in self.bindings_map[action][0]:
                     self.bindings_map[action][2](self, game)
+
+        # Controller events
+        if event.type == pygame.JOYBUTTONDOWN:
+            for action, bindings in self.controller_bindings.items():
+                for binding in bindings:
+                    if binding[0] == 'button' and binding[1] == event.button:
+                        self.bindings_map[action][1](self, game)
+
+        if event.type == pygame.JOYBUTTONUP:
+            for action, bindings in self.controller_bindings.items():
+                for binding in bindings:
+                    if binding[0] == 'button' and binding[1] == event.button:
+                        self.bindings_map[action][2](self, game)
+
+        if event.type == pygame.JOYAXISMOTION:
+            axis = event.axis
+            value = event.value
+
+            if axis in [0, 1]:  # Left stick X and Y
+                prev_value = self.axis_states.get((event.joy, axis), 0)
+                self.axis_states[(event.joy, axis)] = value
+
+                if axis == 0:
+                    # Left direction
+                    if prev_value > -self.joystick_deadzone and value <= -self.joystick_deadzone:
+                        self.bindings_map['left'][1](self, game)
+                    elif prev_value <= -self.joystick_deadzone and value > -self.joystick_deadzone:
+                        self.bindings_map['left'][2](self, game)
+
+                    # Right direction
+                    if prev_value < self.joystick_deadzone and value >= self.joystick_deadzone:
+                        self.bindings_map['right'][1](self, game)
+                    elif prev_value >= self.joystick_deadzone and value < self.joystick_deadzone:
+                        self.bindings_map['right'][2](self, game)
